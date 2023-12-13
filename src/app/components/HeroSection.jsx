60f224548fecc03ed9951e4208bd528667e3e3ea
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-12-13 17:20:25 +0100

    changed image div mobile size

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -48,12 +48,13 @@ const HeroSection = () => {
 						</button>
 					</div>
 				</div>
-				<div className="col-span-5 place-self-center mt-5 mb-6 lg:mt-0">
-					<div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]  relative">
+				<div className="col-span-5 place-self-center mt-5 lg:mt-0">
+					<div className="rounded-full bg-[#181818] w-[350px] h-[350px] lg:w-[400px] lg:h-[400px] relative">
 						<Image
 							src={MY_IMAGE}
 							alt="hero image"
 							width={300}
+							height={300}
 							className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
 						/>
 					</div>
