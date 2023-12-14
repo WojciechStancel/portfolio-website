b272761d6330648d08304f5e3ed48f3bec888c1d
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-12-14 15:56:13 +0100

    build email functionality with Resend

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -4,15 +4,24 @@ import { NextResponse } from "next/server";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 const toEmail = process.env.TO_EMAIL;
-export async function POST() {
+export async function POST(req, res) {
+	const { email, subject, message } = await req.json();
+
 	try {
 		const data = await resend.emails.send({
 			from: fromEmail,
-			to: [toEmail],
-			subject: "Hello world",
+			to: [email, fromEmail],
+			subject: "New message",
 			react: (
 				<>
-					<p>Email Body</p>
+					<h1>Subject: {subject}</h1>
+					<h3>
+						Thank you for email! I will get back with you as soon as I can.
+					</h3>
+					<h4>All the best, Wojtek 😊</h4>
+					{"--------------------------------------------------"}
+					<p>New message from: {email} </p>
+					<p>{message}</p>
 				</>
 			),
 		});
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,10 +1,42 @@
+"use client";
+
 import Link from "next/link";
 import GITHUB_ICON from "../../../public/images/github.svg";
 import INSTAGRAM_ICON from "../../../public/images/instagram.svg";
 import LINKEDIN_ICON from "../../../public/images/linkedin.svg";
 import Image from "next/image";
+import { useState } from "react";
 
 const EmailSection = () => {
+	const [emailSubmitted, setEmailSubmitted] = useState(false);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const data = {
+			email: e.target.email.value,
+			subject: e.target.subject.value,
+			message: e.target.message.value,
+		};
+		const JSONdata = JSON.stringify(data);
+		const endpoint = "/api/send";
+
+		const options = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSONdata,
+		};
+
+		const response = await fetch(endpoint, options);
+		const resData = await response.json();
+
+		if (response.status === 200) {
+			console.log("Message sent.");
+			setEmailSubmitted(true);
+		}
+	};
+
 	return (
 		<section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
 			<div>
@@ -28,11 +60,12 @@ const EmailSection = () => {
 				</div>
 			</div>
 			<div>
-				<form className="flex flex-col gap-6">
+				<form onSubmit={handleSubmit} className="flex flex-col gap-6">
 					<label htmlFor="email" className="text-white block mb-1 font-medium">
 						Your email:
 					</label>
 					<input
+						name="email"
 						type="email"
 						id="email"
 						required
@@ -45,6 +78,7 @@ const EmailSection = () => {
 						Subject:
 					</label>
 					<input
+						name="subject"
 						type="text"
 						id="subject"
 						required
@@ -69,6 +103,11 @@ const EmailSection = () => {
 						className="px-5 py-2.5 w-full rounded-md bg-gradient-to-br from-blue-300 via-blue-500 to-red-400 hover:bg-red-900 text-white font-medium">
 						Send Message
 					</button>
+					{emailSubmitted && (
+						<p className="text-green-400 text-sm mt-2">
+							Successfully sent your email!
+						</p>
+					)}
 				</form>
 			</div>
 		</section>
