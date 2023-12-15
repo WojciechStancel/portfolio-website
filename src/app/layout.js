5ad012014ae538577a6624d03980e9791954de9d
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-12-15 14:59:23 +0100

    updated favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,13 +4,22 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "Portfolio",
+	title: "Portfolio Website - Wojciech Stancel ",
 	description: "Portfolio website generated with Next.js",
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" style={{ scrollBehavior: "smooth" }}>
+			<head>
+				<link rel="icon" href="/favicon.ico" sizes="any" />
+				<link
+					rel="apple-touch-icon"
+					href="/apple-icon?<generated>"
+					type="image/<generated>"
+					sizes="<generated>"
+				/>
+			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
